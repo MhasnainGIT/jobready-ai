37f5fd48479e31fcd1b4e98102ee55122faa5d19
MhasnainGIT <mohdhasnain1544@gmail.com>
2025-06-25 21:28:37 +0530

    last change

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {   
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
